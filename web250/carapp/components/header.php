7d5c91e648d0b57dff02ce3f86d7937e025abbca
Salah <187458641+Sal-ah-cmd@users.noreply.github.com>
2025-11-01 13:51:53 -0400

    Add files via upload

---
+++ web250/carapp/components/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sneaky Snake's Used Cars</title>
+    <link rel="stylesheet" href="styles/default.css">
+</head>
+<body>
+    <header>
+        <h1> Sneaky Snake's Used Cars </h1>
+    </header>
+    <main>
