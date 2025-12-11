6603e3941c9d3c7ea87695b1e8500a3e9cc0e50a
Salah <187458641+Sal-ah-cmd@users.noreply.github.com>
2025-12-11 03:27:24 -0500

    Delete web250 directory

--- web250/carapp2/components/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Designed by The Sneaky Firm. All rights reserved.</p>
-        <p>Disclaimer: Shhhhh Sneaky prices on used cars.</p>
-    </footer>
-</body>
-</html>
